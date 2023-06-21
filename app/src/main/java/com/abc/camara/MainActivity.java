329a404ea5934d4ef7ec2a1bcbc43d7ede3dbe13
Lesli16 <mishel03llango@gmail.com>
2023-06-20 20:47:14 -0500

    Se agrega el proyecto

---
+++ app/src/main/java/com/abc/camara/MainActivity.java
@@ -0,0 +1,65 @@
+package com.abc.camara;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Bitmap imageBitmap;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void takePicture(View view) {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void saveImage(View view) {
+        if (imageBitmap != null) {
+            String savedImageURL = MediaStore.Images.Media.insertImage(
+                    getContentResolver(),
+                    imageBitmap,
+                    "Imagen capturada",
+                    "Descripción de la imagen"
+            );
+            Toast.makeText(this,"GUARDADO EXITOSO!", Toast.LENGTH_LONG).show();
+            if (savedImageURL != null) {
+
+            } else {
+
+            }
+        } else {
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+
+        }
+    }
+
+}
+
+
+
+
